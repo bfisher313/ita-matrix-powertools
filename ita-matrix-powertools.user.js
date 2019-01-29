0d6ee645151461c90a06af231b70c1d48d937c53
brian <bpfisher@gmail.com>
2019-01-28 21:21:50 -0500

    Initial attempt at displaying credit calculation results

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -2,7 +2,7 @@
 // @name ITA-Matrix-Powertools
 // @namespace https://github.com/bfisher313/ita-matrix-powertools
 // @description Adds new features and builds fare purchase links for ITA Matrix
-// @version 0.50.1.041
+// @version 0.50.1.042
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant GM.getValue
 // @grant GM_setValue
@@ -3810,8 +3810,8 @@ function printFlightCreditCalculatorResults(data) {
                         output += '<td style="text-align: center; border: 1px solid black;">CPEQP</td>';
                         output += '<td style="text-align: center; border: 1px solid black;">' + creditOption.redeemablePointsLabel + '</td>';
                         output += '<td style="text-align: center; border: 1px solid black;">CPRDM</td></tr>';
-                        output += '<tr><td style="text-align: center; border: 1px solid black;">' + creditOption.eliteQualfyingPoints + '</td>';
-                        output += '<td style="text-align: center; border: 1px solid black;">' + creditOption.costPerEliteQualiyingPoint + '</td>';
+                        output += '<tr><td style="text-align: center; border: 1px solid black;">' + creditOption.eliteQualifyingPoints + '</td>';
+                        output += '<td style="text-align: center; border: 1px solid black;">' + creditOption.costPerEliteQualfiyingPoint + '</td>';
                         output += '<td style="text-align: center; border: 1px solid black;">' + creditOption.redeemablePoints + '</td>';
                         output += '<td style="text-align: center; border: 1px solid black;">' + creditOption.costPerRedeemablePoint + '</td></tr>';
                         output += "</tbody></table>";
