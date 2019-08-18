1ade2255268cbf9b6b766c4e54fda88668f854f1
bfisher <bpfisher@gmail.com>
2019-08-18 17:54:11 -0400

    Updates to add dynamic labels for the Cost Per EQMs, EQDs, and RDMs

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -2,7 +2,7 @@
 // @name ITA-Matrix-Powertools
 // @namespace https://github.com/bfisher313/ita-matrix-powertools
 // @description Adds new features and builds fare purchase links for ITA Matrix
-// @version 0.50.1.54
+// @version 0.50.1.55
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant GM.getValue
 // @grant GM_setValue
@@ -3805,11 +3805,11 @@ function printFlightCreditCalculatorResults(data) {
                         output += '<tr><td colspan="3" style="text-align: center;">' + creditOption.earningAirlineName + '</td>';
                         output += '<td colspan="3" style="text-align: center;">Actual Miles Flown: ' + creditOption.actualMilesFlown + '</td></tr>';
                         output += '<tr><td style="text-align: center;">' + creditOption.eliteQualifyingPointsLabel + '</td>';
-                        output += '<td style="text-align: center;">CPEQP</td>';
+                        output += '<td style="text-align: center;">' + creditOption.costPerEliteQualifyingPointLabel + '</td>';
                         output += '<td style="text-align: center;">' + creditOption.redeemablePointsLabel + '</td>';
-                        output += '<td style="text-align: center;">CPRDM</td>';
+                        output += '<td style="text-align: center;">' + creditOption.costPerRedeemablePointLabel + '</td>';
                         output += '<td style="text-align: center;">' + creditOption.eliteQualifyingDollarsLabel + '</td>';
-                        output += '<td style="text-align: center;">CPEQD</td></tr>';
+                        output += '<td style="text-align: center;">' + creditOption.costPerEliteQualifyingDollarLabel + '</td></tr>';
                         output += '<tr><td style="text-align: center;">' + creditOption.eliteQualifyingPoints + '</td>';
                         output += '<td style="text-align: center;">' + creditOption.costPerEliteQualifyingPoint + '</td>';
                         output += '<td style="text-align: center;">' + creditOption.redeemablePoints + '</td>';
